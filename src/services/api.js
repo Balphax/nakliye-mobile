0bb90b705459f24479f84138f96b702b1c7dfab3
Balphax <burakekiciogluu@gmail.com>
2026-04-28 19:52:41 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://YOUR_RENDER_URL'; // Render URL'ini buraya yaz
+const API_URL = 'https://nakliye-bot-v3.onrender.com'; // Render URL'ini buraya yaz
 
 const api = axios.create({
   baseURL: API_URL,
